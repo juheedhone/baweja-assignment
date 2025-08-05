4145dd7e03c4b535dbf01c03c84eeeb7fc3ce62c
juheedhone <juheedhone12@gmail.com>
2025-08-06 00:02:39 +0530

    task: improve home page ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,6 @@
+import { CalendarIcon, GitHubLogoIcon } from "@radix-ui/react-icons";
 import { Link } from "react-router";
+import { Button } from "~/components/ui/button";
 import type { Route } from "./+types/home";
 
 export function meta(meta: Route.MetaArgs) {
@@ -10,23 +12,35 @@ export function meta(meta: Route.MetaArgs) {
 
 export default function Home() {
 	return (
-		<>
-			<Link to={"/todo"}>
-				<button
-					type="button"
-					className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-				>
-					TODO
-				</button>
-			</Link>
-			<Link to={"/github"}>
-				<button
-					type="button"
-					className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-				>
-					GitHub
-				</button>
-			</Link>
-		</>
+		<div className="min-h-[calc(100vh-2rem)] flex flex-col items-center justify-center space-y-8 p-8">
+			<div className="space-y-4 text-center">
+				<h1 className="text-4xl font-bold tracking-tight">
+					Select where you'd like to go!
+				</h1>
+			</div>
+
+			<div className="flex flex-col gap-4 sm:flex-row">
+				<Link to={"/todo"}>
+					<Button
+						size="lg"
+						className="w-[200px] transition-transform hover:scale-105"
+						variant="default"
+					>
+						<CalendarIcon className="w-5 h-5 mr-2" />
+						Todo App
+					</Button>
+				</Link>
+				<Link to={"/github"}>
+					<Button
+						size="lg"
+						className="w-[200px] transition-transform hover:scale-105"
+						variant="outline"
+					>
+						<GitHubLogoIcon className="w-5 h-5 mr-2" />
+						GitHub
+					</Button>
+				</Link>
+			</div>
+		</div>
 	);
 }
