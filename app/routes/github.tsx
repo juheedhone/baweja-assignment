0566e7386ce677fcb2f4c4a4bd3409527a154531
juheedhone <juheedhone12@gmail.com>
2025-08-05 18:57:14 +0530

    task:fetch data.

--- app/routes/github.tsx
+++ app/routes/github.tsx
@@ -1,5 +1,20 @@
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import axios from "axios";
+import { useEffect } from "react";
+
 const Github = () => {
-  return <div>Github</div>;
+  useEffect(() => {
+    axios
+      .get("https://api.github.com/users/facebook")
+      .then((res) => console.log(res.data));
+  }, []);
+
+  return (
+    <div>
+      <input className="border border-gray-400 rounded-md" type="text" id="text" ></input>
+      <button type="button">{<MagnifyingGlassIcon />}</button>
+    </div>
+  );
 };
 
 export default Github;
