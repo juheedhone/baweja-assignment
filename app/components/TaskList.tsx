ac9ba7064f359b1ad7892175d98073306479722a
juheedhone <juheedhone12@gmail.com>
2025-08-05 23:47:44 +0530

    task: add search bar

--- app/components/TaskList.tsx
+++ app/components/TaskList.tsx
@@ -1,11 +1,14 @@
-import { TrashIcon } from "@radix-ui/react-icons";
+import { MagnifyingGlassIcon, TrashIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeItem } from "~/redux/slice/todoSlice";
 import type { RootState } from "~/redux/store";
 import { Button } from "./ui/button";
+import { Input } from "./ui/input";
 
 const TaskList = () => {
   const todos = useSelector((state: RootState) => state.todoSlice.items);
+  const [searchQuery, setSearchQuery] = useState("");
 
   const dispatch = useDispatch();
 
@@ -13,9 +16,25 @@ const TaskList = () => {
     dispatch(removeItem(id));
   };
 
+  const filteredTodos = todos.filter((todo) =>
+    todo.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="mt-20 space-y-4">
-      {todos.map((t) => (
+      <div className="flex justify-end w-[80%] mx-auto mb-4">
+        <div className="relative w-64">
+          <Input
+            type="text"
+            placeholder="Search tasks..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="pr-8"
+          />
+          <MagnifyingGlassIcon className="absolute text-gray-500 -translate-y-1/2 right-2 top-1/2 size-4" />
+        </div>
+      </div>
+      {filteredTodos.map((t) => (
         <div
           className="flex justify-between items-center p-4 border border-gray-200 hover:border-gray-500 h-20 shadow rounded-xl w-[80%] mx-auto"
           key={t.id}
